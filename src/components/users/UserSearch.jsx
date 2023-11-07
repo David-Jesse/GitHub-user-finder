a85702d34bcc87b0ea579cd6570c9cfcda0d9093
David-Jesse <djesse351@gmail.com>
2023-11-07 12:44:14 -0800

    cleaned my githubactions file and made finishing touches to my work

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -6,7 +6,7 @@ import { searchUsers } from "../../context/github/GithubActions"
 function UserSearch() {
     const [text, setText] = useState('')
 
-    const {users, clearUsers, dispatch} = useContext(GithubContext)
+    const {users, dispatch} = useContext(GithubContext)
 
     const {setAlert} = useContext(AlertContext)
 
@@ -46,7 +46,7 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-        <button className="btn btn-ghost btn-lg" onClick={clearUsers}>
+        <button className="btn btn-ghost btn-lg" onClick={() => dispatch({type: 'CLEAR_USERS'})}>
             Clear
         </button>
       </div>
